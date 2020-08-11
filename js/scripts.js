38d99af86a2c5d957979ee0678043240da66cbdc
CameronWill <cellardoorsolar@aol.com>
2020-08-11 10:32:01 -0700

    attempt fade and slide functions
    Co-Authored-By: Garrett Brown <gman99mm@live.com>

--- js/scripts.js
+++ js/scripts.js
@@ -11,3 +11,27 @@ $(document).ready(function() {
     $("#emu-hidden").toggle();
   });
 });
+
+$(document).click(function(){
+  $("#walrus-hidden").fadeTo("#walrus-showing");
+  $("#walrus-showing").fadeToggle("slow");
+});
+
+$(document).click(function(){
+  $("#emu-hidden").fadeTo("#emu-showing");
+  $("#emu-showing").fadeToggle("slow");
+});
+
+$("#flip").click(function(){
+  $("#panel").slideUp();
+});
+
+$("#flip").click(function(){
+  $("#panel").slideDown();
+});
+
+$("#flip").click(function(){
+  $("#panel").slideToggle();
+});
+
+
