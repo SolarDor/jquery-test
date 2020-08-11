f8aaa0d8d376029f4a85ecf519a9b632af6c7b97
CameronWill <cellardoorsolar@aol.com>
2020-08-11 13:25:02 -0700

    make slide/fade/hide/show function properly as separates
    Co-Authored-By: Garrett D Brown <gman9mm@live.com>

--- js/scripts.js
+++ js/scripts.js
@@ -1,37 +1,35 @@
 $(document).ready(function() {
   $(".clickable").click(function() {
-    $("#walrus-showing").toggle();
-    $("#walrus-hidden").toggle();
+    $("#walrus-showing").slideToggle();
+    $("#walrus-hidden").slideToggle(fast);
   });
-});
 
-$(document).ready(function() {
   $(".clickable1").click(function() {
-    $("#emu-showing").toggle();
-    $("#emu-hidden").toggle();
+    $("#emu-showing").slideToggle();
+    $("#emu-hidden").slideToggle(fast);
   });
 });
 
-$(document).click(function(){
-  $("#walrus-hidden").fadeTo("#walrus-showing");
-  $("#walrus-showing").fadeToggle("slow");
-});
+// $(".clickable").click(function(){
+//   $("#walrus-hidden").fadeTo();
+//   $("#walrus-showing").fadeToggle("slow");
+// });
 
-$(document).click(function(){
-  $("#emu-hidden").fadeTo("#emu-showing");
-  $("#emu-showing").fadeToggle("slow");
-});
+// $(".clickable1").click(function(){
+//   $("#emu-hidden").fadeTo();
+//   $("#emu-showing").fadeToggle("slow");
+// });
 
-$(document).click(function(){
-  $(".slide").slideUp();
-});
+// $(document).click(function(){
+//   $(".slide").slideUp();
+// });
 
-$("#flip").click(function(){
-  $("#panel").slideDown();
-});
+// $("#flip").click(function(){
+//   $("#panel").slideDown();
+// });
 
-$("#flip").click(function(){
-  $("#panel").slideToggle();
-});
+// $("#flip").click(function(){
+//   $("#panel").slideToggle();
+// });
 
 
