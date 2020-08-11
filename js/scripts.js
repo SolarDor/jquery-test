948d3e31ea65f9690efdbf7c55340b9b3583af2f
CameronWill <cellardoorsolar@aol.com>
2020-08-11 09:53:27 -0700

    will add emu, hide/show functions, add stylings and scripts
    Co-Authored-By: Garrett Brown <gman99mm@live.com>

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,13 @@
 $(document).ready(function() {
   $(".clickable").click(function() {
-    $(".walrus-showing").toggle();
-    $(".walrus-hidden").toggle();
+    $("#walrus-showing").toggle();
+    $("#walrus-hidden").toggle();
+  });
+});
+
+$(document).ready(function() {
+  $(".clickable1").click(function() {
+    $("#emu-showing").toggle();
+    $("#emu-hidden").toggle();
   });
 });
